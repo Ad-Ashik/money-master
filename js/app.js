e31a24731a0789d77f5e0d64e5181060407c33b2
AIA <adashik585@gmail.com>
2022-02-17 23:05:07 +0600

    Saving Amount added

--- js/app.js
+++ js/app.js
@@ -7,18 +7,6 @@ function getInputValue(price) {
 
 }
 
-// salary
-function salarySave(totalExpens) {
-    // const salaryInput = getInputValue('salary');
-    // const subTotal = salaryInput - totalExpens;
-    if (salaryInput > 0) {
-        return subTotal;
-    }
-    else {
-        alert('please valid number');
-    }
-
-}
 
 // calculate button evevt
 document.getElementById('calculate').addEventListener('click', function () {
@@ -26,18 +14,17 @@ document.getElementById('calculate').addEventListener('click', function () {
     const rentInput = getInputValue('rent');
     const clothesInput = getInputValue('clothes');
     const totalExpens = foodInput + rentInput + clothesInput;
-    // document.getElementById('total-expens').innerText = totalExpens;
 
     // salary
     const salaryInput = getInputValue('salary');
     const subTotal = salaryInput - totalExpens;
-    // document.getElementById('total-salary').innerText = subTotal;
 
     if (foodInput >= 0 && rentInput >= 0 && clothesInput >= 0 && salaryInput > 0) {
         document.getElementById('total-expens').innerText = totalExpens;
         document.getElementById('total-salary').innerText = subTotal;
-
-
+    }
+    else if (salaryInput < totalExpens) {
+        alert('your income is not  enough')
     }
     else {
         alert('please valid number');
@@ -49,14 +36,19 @@ document.getElementById('saving-button').addEventListener('click', function () {
     const salaryInput = getInputValue('salary');
     const saveInput = getInputValue('save');
     const saving = (salaryInput * saveInput) / 100;
+    if (salaryInput > 0) {
+        document.getElementById('saving-amount').innerText = saving;
+    }
+    else {
+        alert('please valid number');
+    }
+
 
     // save
     const totalSalary = parseInt(document.getElementById('total-salary').innerText);
     const savingAmount = parseInt(document.getElementById('saving-amount').innerText);
-    const totalBalance = totalSalary - savingAmount;
-    if (salaryInput > 0) {
-        document.getElementById('saving-amount').innerText = saving;
-        document.getElementById('total-balance').innerText = totalBalance;
+    if (totalSalary > 0) {
+        document.getElementById('total-balance').innerText = totalSalary - savingAmount;
     }
     else {
         alert('please valid number');
